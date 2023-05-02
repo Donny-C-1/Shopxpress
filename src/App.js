305c93d5c9dad9230c8f6ff7315977b1fd3c392c
Donny C <99075588+Donny-C-1@users.noreply.github.com>
2023-05-02 20:41:47 +0100

    Added routing to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/nav/nav";
 import Header from "./components/header/header";
 import Homepage from "./components/homepage/homepage";
 import Footer from "./components/footer/footer";
+import Cart from "./components/cart/cart";
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Header />
-      <Homepage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={(
+            <div>
+              <Header />
+              <Homepage />
+            </div>
+          )} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
---
+++ src/components/cart/cart.js
@@ -0,0 +1,9 @@
+function Cart() {
+    return (
+        <div>
+            <p>This is the cart page</p>
+        </div>
+    )
+}
+
+export default Cart;
