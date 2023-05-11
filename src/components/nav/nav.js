7251a7ad46d742ecf5821e86a88d90a48eaff58b
Donny C <99075588+Donny-C-1@users.noreply.github.com>
2023-05-11 19:50:39 +0100

    Completed the navbar

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -33,7 +33,7 @@ function Nav() {
                 </div>
             </div>
             <nav className={"top-nav" + (menuVisibility ? " visible" : "")}>
-                <div class="nav-inner">
+                <div className="nav-inner">
                     <div className="logo">
                         <img src={logo} alt="logo" width="100%" height="auto" />
                         <span>Shopxpress</span>
@@ -48,7 +48,11 @@ function Nav() {
                         <a href="/#">Delivery</a>
                         <form className='input-bar'>
                             <input type="text" placeholder="Search Product" />
-                            <button type="submit">Search</button>
+                            <button type="submit">
+                                <svg width="100%" viewBox="0 0 24 24">
+                                    <use href="#search-icon" />
+                                </svg>
+                            </button>
                         </form>
                         <a href="/#" className="side-link">Account</a>
                         <a href="/#" className="side-link">Cart</a>
@@ -56,7 +60,11 @@ function Nav() {
 
                     <div className={"mobile-input-bar" + (searchVisibility ? " visible" : "")}>
                         <input type="text" placeholder="Search..." autoComplete="false" />
-                        <input type="submit" value="search" />
+                        <button type="submit">
+                            <svg width="100%" viewBox="0 0 24 24">
+                                <use href="#search-icon" />
+                            </svg>
+                        </button>
                     </div>
 
                     <div className="mobile-buttons">
@@ -64,7 +72,7 @@ function Nav() {
                         <a href="/#" className="side-link">Cart</a>
                         <button className="search-button" onClick={toggleSearchInput}>
                             <svg width="100%" viewBox="0 0 24 24">
-                                <path d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
+                                <path id="search-icon" fill="currentColor" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
                             </svg>
                         </button>
                         <button className="animated-menu-button" onClick={toggleMenuVisibility}>
